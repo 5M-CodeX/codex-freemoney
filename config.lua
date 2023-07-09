2c57a20abe656c0b67f31ef1d65369a6bf5616c2
Mehdi <57235093+Mehdi-MMO@users.noreply.github.com>
2023-07-09 13:35:33 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- Cooldown duration in seconds
+Config.cooldownDuration = 3600
+
+-- Amount of money to give
+Config.moneyAmount = 1000
+
+-- Cooldown message
+Config.cooldownMessage = "^1You can only use this command once every hour. Please wait %d seconds."
+
+-- Success message
+Config.successMessage = "^2You received some free money!"
